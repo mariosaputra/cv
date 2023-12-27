fc011b48d88f50a2f8e30a668f892400a80c75d6
Mario Saputra <mariojp26@gmail.com>
2023-12-27 20:51:36 +0900

    update: init data

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,259 +20,139 @@ import { LinkedInIcon } from "../components/icons/LinkedInIcon";
 import { XIcon } from "../components/icons/XIcon";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
-  about:
-    "Full Stack Engineer focused on building products with extra attention to details",
+  name: "Mario Saputra",
+  initials: "MS",
+  location: "Osaka, Japan",
+  locationLink: "https://www.google.com/maps/place/Osaka",
+  about: "Web Developer | iOS Developer",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "Hi, I'm a Programmer with experience in various roles, from Firmware Engineering to Web and Mobile Development. Right now, I'm working as a full-time Web Developer and also doing my own projects in my free time. I'm open to collaborating on projects or freelance work. Feel free to get in touch!",
+  avatarUrl:
+    "https://res.cloudinary.com/mariosaputra-me/image/upload/v1703666346/MS_kg34cv_xuzj7e.jpg",
+  personalWebsiteUrl: "https://marioapps.com",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "mariosaputra@gmail.com",
+    tel: "+817021789987",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/mariosaputra",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/mario-saputra-jp/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://twitter.com/mario_apps_dev",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Waseda University",
+      degree: "Master Degree in Computer Science and Communication Engineering",
+      start: "Sep 2014",
+      end: "Sep 2016",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
+      company: "Future Spirits",
+      link: "https://www.future-s.com/",
       badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
+      title: "Full Stack Developer",
+      // logo: ParabolLogo,
+      start: "Nov 2022",
+      end: "Present",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+        "Worked on a website with a large user base, maintained and added new features, replaced servers, and started migration to Docker, Single Page Applications (SPA), and more. Technologies: JavaScript · MySQL · Vue.JS · PHP · Perl ·  Linux · Docker ",
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
+      company: "Adecco",
+      link: "https://www.adecco.co.jp/",
+      badges: ["On-site"],
+      title: "Software Engineer",
+      // logo: ClevertechLogo,
+      start: "Aug 2019",
+      end: "Oct 2022",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+        "Worked on-site at a client's office as a dispatched employee, developing a 3D Robot Simulation Software for Windows. Technologies: .NET Framework · C# · C++",
     },
     {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
+      company: "Xacti",
+      link: "https://xacti-co.com/",
       badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      title: "Firmware Engineer",
+      // logo: JojoMobileLogo,
+      start: "Apr 2014",
+      end: "Jul 2019",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+        "Developed firmware for digital cameras, led the GUI development team, and worked on various projects including an object detection camera, a music recorder, a night vision camera, and more. Technologies: C · Python",
     },
   ],
   skills: [
     "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
+    "PHP",
+    "Python",
+    "Vue/Nuxt.js/JQuery",
+    "Swift/SwiftUI",
     "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "MySQL",
+    "Linux Server",
+    "C/C++/C#",
   ],
   projects: [
     {
-      title: "Consultly",
-      techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
-      ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
+      title: "PixAsset.com",
+      techStack: ["Side Project", "Vue3", "Express.js", "OpenAI", "Firebase"],
+      description:
+        "Web app to generate stunning pixel art assets for your projects",
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "pixasset.com",
+        href: "https://pixasset.com/",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      title: "Macro Calculator AI",
+      techStack: ["Side Project", "iOS", "SwiftUI", "Flask", "OpenAI"],
       description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
+        "iOS app to get total macro percentages and calories for any food, powered by AI",
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "Macro Calculator AI App",
+        href: "https://apps.apple.com/us/app/macro-calculator-ai/id6469041577",
       },
     },
     {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
+      title: "MediLogger",
+      techStack: ["Side Project", "iOS", "SwiftUI", "CloudKit"],
+      description: "Easy medical data logging for iOS",
       link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
+        label: "MediLogger App",
+        href: "https://apps.apple.com/us/app/medical-logger-medilogger/id6466779296",
       },
     },
     {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      title: "Moving Calories Calculator",
+      techStack: ["Side Project", "iOS", "SwiftUI"],
       description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
+        "Highly accurate calculator to count calories burned from various activities",
       link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
+        label: "Moving Calories Calculator App",
+        href: "https://apps.apple.com/us/app/moving-calories-calculator/id6456799256",
       },
     },
     {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
+      title: "QuickPassGen",
+      techStack: ["Side Project", "iOS", "SwiftUI"],
+      description: "iOS app to generate random password",
       link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
+        label: "QuickPassGen App",
+        href: "https://apps.apple.com/us/app/quickpassgen/id6451387153",
       },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
